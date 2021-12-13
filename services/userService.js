0e91ea969f317f3317a51322c9ff058eb6ac12a1
Juho <juho.kanerva@gmail.com>
2021-12-13 10:26:51 -0800

    fix registration

--- services/userService.js
+++ services/userService.js
@@ -2,7 +2,7 @@ import { executeQuery } from "../database/database.js";
 
 const addUser = async (email, password) => {
   await executeQuery(
-    `INSERT INTO users (email, password) VALUES ($1, $2, $3, $4)`,
+    `INSERT INTO users (email, password) VALUES ($1, $2)`,
     email,
     password,
   );
